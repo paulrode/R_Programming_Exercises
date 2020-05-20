dce4fe62d7e11ae64ad9bb058f8f9f7e2bb58735
paulrode <paulrode@outlook.com>
2020-05-19 21:10:00 -0400

    First commit

---
+++ corr.R
@@ -0,0 +1,32 @@
+#Load Tidy enviroment 
+library("tidyverse")
+library("dplyr")
+library("stringr")
+library("lubridate")
+library("knitr")
+library("kableExtra")
+library("fpp2")
+library("readxl")
+
+#Define Function
+complete <- function(directory, id = 1:332) 
+{
+  answer <- 0
+  nobs <- 0
+  file_data <- 0
+  files <- 0
+  path <- 0
+  path <- paste(getwd(), directory, sep = "/")
+  files <-  list.files(path) 
+  for (i in 1:length(id)) 
+  {
+    file_data <- read_csv(paste(path, files[id[i]], sep = "/"))
+    nobs[i] <- nrow(file_data[complete.cases(file_data),])
+  }
+  answer <- data.frame(id,nobs)
+  return(answer)
+}
+
+#Run Function
+complete("specdata",c(2,4,8,10,12))
+

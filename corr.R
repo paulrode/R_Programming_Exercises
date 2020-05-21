aafd3795f324db6ac84914305df80356d6472273
paulrode <paulrode@outlook.com>
2020-05-20 21:00:26 -0400

    first draft run

--- corr.R
+++ corr.R
@@ -9,24 +9,26 @@ library("fpp2")
 library("readxl")
 
 #Define Function
-complete <- function(directory, id = 1:332) 
+corr <- function(directory, threshold = 0) 
 {
-  answer <- 0
-  nobs <- 0
+  cr <- 0
   file_data <- 0
   files <- 0
   path <- 0
   path <- paste(getwd(), directory, sep = "/")
   files <-  list.files(path) 
-  for (i in 1:length(id)) 
+  for (i in 1:length(files)) 
   {
-    file_data <- read_csv(paste(path, files[id[i]], sep = "/"))
-    nobs[i] <- nrow(file_data[complete.cases(file_data),])
+    file_data <- read_csv(paste(path, files[i], sep = "/"))
+    
+    if ( threshold < nrow(file_data[complete.cases(file_data),]))
+    {
+      file_data <- file_data[complete.cases(file_data),]
+      cr[i] <- cor(file_data$nitrate, file_data$sulfate) }    else {}
+    }
+  return(cr)
   }
-  answer <- data.frame(id,nobs)
-  return(answer)
-}
 
 #Run Function
-complete("specdata",c(2,4,8,10,12))
+cr <- corr("specdata",150)
 

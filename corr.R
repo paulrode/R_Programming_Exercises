86fe764971e4168259e5e9dd1bf71879116e6fd4
paulrode <paulrode@outlook.com>
2020-05-20 22:01:06 -0400

    added omit na's

--- corr.R
+++ corr.R
@@ -12,24 +12,30 @@ library("readxl")
 corr <- function(directory, threshold = 0) 
 {
   cr <- 0
+  i <- 1
   file_data <- 0
   files <- 0
   path <- 0
   path <- paste(getwd(), directory, sep = "/")
   files <-  list.files(path) 
+  
   for (i in 1:length(files)) 
   {
     file_data <- read_csv(paste(path, files[i], sep = "/"))
+    file_data <- file_data[complete.cases(file_data), ]
     
-    if ( threshold < nrow(file_data[complete.cases(file_data),]))
+    if ( threshold < nrow(file_data))
     {
-      file_data <- file_data[complete.cases(file_data),]
-      cr[i] <- cor(file_data$nitrate, file_data$sulfate) }    else {}
+    file_data <- file_data[complete.cases(file_data), ]  
+    cr[i] <- cor(file_data$nitrate, file_data$sulfate)
     }
-  return(cr)
   }
+  cr <- na.omit(cr)
+  return(cr[-1])
+}
 
 #Run Function
-cr <- corr("specdata",150)
+cr <- corr("specdata")
 head(cr)
 summary(cr)
+length(cr)

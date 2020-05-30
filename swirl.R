73912bda943e699bd847012e2afb5d4664bfb065
paulrode <paulrode@outlook.com>
2020-05-30 09:47:52 -0400

    Commit update for removing non neeed lines of code

--- swirl.R
+++ swirl.R
@@ -1,5 +1,21 @@
+
 install.packages("swirl")
 library(swirl)
 install_from_swirl("R Programming")
 swirl()
 Paul
+
+3
+
+library(tidyverse)
+library(dplyr)
+library(datasets)
+data(iris)
+?iris
+str(iris)
+vir <- iris[,c("Species", "Sepal.Length")]
+vir
+colMeans(vir, 101:length(vir))
+
+str(vir)
+class(vir)

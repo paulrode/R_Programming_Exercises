fd455f6773c6148e1d693b9695697b78d5cbd760
paulrode <paulrode@outlook.com>
2020-06-02 22:20:50 -0400

    Second commit

--- Assigement 3.R
+++ Assigement 3.R
@@ -1,1 +1,9 @@
- 
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+ncol(outcome)
+nrow(outcome)
+names(outcome)
+outcome[, 11] <- as.numeric(outcome[, 11])
+## You may get a warning about NAs being introduced; that is okay
+hist(outcome[, 11])
+#need to do this section now >>> 2 Finding the best hospital in a state

a5db2bbc35ee48c11f84097b1d2aaf0ee2e34be6
paulrode <paulrode@outlook.com>
2020-06-03 22:08:40 -0400

    Commit of second night's work.

--- Assigement 3.R
+++ Assigement 3.R
@@ -7,3 +7,12 @@ outcome[, 11] <- as.numeric(outcome[, 11])
 ## You may get a warning about NAs being introduced; that is okay
 hist(outcome[, 11])
 #need to do this section now >>> 2 Finding the best hospital in a state
+
+best <- function(state) {
+  ## Read outcome data 
+  outcome[ , outcome$State == state]
+  ## Check that state and outcome are valid
+  
+  ## Return hosputal name in that state with lowest 30-day death rate
+  
+}

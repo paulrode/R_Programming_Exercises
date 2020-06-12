fff3bbed178664ebd689d3d7005c40f80993b866
paulrode <paulrode@outlook.com>
2020-06-12 19:44:42 -0400

    Commit for part 2 complete

--- Assigement 3.R
+++ Assigement 3.R
@@ -7,7 +7,7 @@ library("fpp2")
 library("readxl")
 
 
-
+#Part 1
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 head(outcome)
 ncol(outcome)
@@ -18,37 +18,32 @@ outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
 
 
+
+#Part 2
 #need to do this section now >>> 2 Finding the best hospital in a state
 
 
 best <- function(state, outcome) {
   
-  state <- "GA"
-  outcome <- "Pneumonia"
-  
   ## Read outcome data 
-  outcome1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  outcome1 <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
   outcome1 <- outcome1[ , c(2,6,7,11, 17, 23)]
-  names(outcome1) <- c("Hospital", "City", "State", "Heart Failure", "Heart Attack", "Pneumonia")
+  names(outcome1) <- c("Hospital", "City", "State", "heart failure", "heart attack", "pneumonia")
  
   ## Check that state and outcome are valid
- 
   if(any(state == outcome1$State, na.rm = TRUE) == "FALSE") { stop("invalid state" )}
-  if(any(outcome == c("Heart Failure", "Heart Attack", "Pneumonia"), na.rm = TRUE) == "FALSE") {
+  if(any(outcome == c("heart failure", "heart attack", "pneumonia"), na.rm = TRUE) == "FALSE") {
     stop("invalid outcome") } 
   
-  
-  ## Return hosputal name in that state with lowest 30-day death rate
-  filter(outcome1, State == state) %>% select(Hospital, City, State, outcome) %>% na.omit() -> outcome2
-  as.numeric(outcome2[,4]) -> outcome2[ ,4]
-  na.omit(outcome2)
-  outcome2 %>% arrange(outcome2[ , 4]) %>% arrange(outcome2[ , 1]) -> outcome2
-  print(outcome2[1,])
+  ## Return hospital name in that state with lowest 30-day death rate
+  filter(outcome1, State == state) %>% select(c("Hospital", outcome)) %>% na.omit()-> outcome1
+  outcome1 %>% arrange(outcome1[ ,2], Hospital) -> outcome1
+  print(outcome1[1:5,])
   }
-answer <- best("GA", "Pneumonia")
 
-answer
+best("TX", "heart attack")
 
-head(answer) 
+
+ 
 
 

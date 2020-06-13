21cb08c386dc45ad2e38ea399a368bebbfa60e10
paulrode <paulrode@outlook.com>
2020-06-12 20:56:01 -0400

    added part 4

--- Assigement 3.R
+++ Assigement 3.R
@@ -64,19 +64,38 @@ rankhospital <- function(state, outcome, num = "best") {
   ## Return hospital name in that state with the given rank
     filter(outcome1, State == state) %>% select(c("Hospital", outcome)) %>% na.omit()-> outcome1
     outcome1 %>% arrange(outcome1[ ,2], Hospital) -> outcome1
-    print(outcome1[1:5,])
+    if(num == "best") { num <- 1} 
+    if(num == "worse") { num <- nrow(outcome1)}
+    if(num > nrow(outcome1)) {print("NA")} else {print(outcome1[num, ])}
     
     }
-  
 
   
-  
-rankhospital("MD", "heart failure", 5)
+rankhospital("MD", "heart failure", "worse")
+rankhospital("MN", "heart attack", 5000)
 
-rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
+
+# Part 4  file: rankall.R
+
+rankall <- function(outcome, num = "best") {
+  
+  ## Read outcome data 
+    outcome1 <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+    outcome1 <- outcome1[ , c(2,7,11, 17, 23)]
+    names(outcome1) <- c("Hospital", "State", "heart failure", "heart attack", "pneumonia")
+  
   ## Check that state and outcome are valid
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
+    if(any(outcome == c("heart failure", "heart attack", "pneumonia"), na.rm = TRUE) == "FALSE") {
+    stop("invalid outcome") } 
   
+  ## Return hospital name in that state with the given rank
+    outcome1 %>% select(c("Hospital", outcome)) %>% na.omit()-> outcome1
+    outcome1 %>% group_by("State")
+    outcome1 %>% arrange(outcome1[ ,2], Hospital) -> outcome1
+    if(num == "best") { num <- 1} 
+    if(num == "worse") { num <- nrow(outcome1)}
+    if(num > nrow(outcome1)) {print("NA")} else {print(outcome1[num, ])}
+}
+
+
 

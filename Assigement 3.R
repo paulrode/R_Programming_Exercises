4f85e5642cd09df48dddc0cd1d462c0ad496d1c7
paulrode <paulrode@outlook.com>
2020-06-12 20:32:31 -0400

    Added working part of part 3 of problem

--- Assigement 3.R
+++ Assigement 3.R
@@ -19,7 +19,7 @@ hist(outcome[, 11])
 
 
 
-#Part 2
+#Part 2 file: best.R
 #need to do this section now >>> 2 Finding the best hospital in a state
 
 
@@ -44,6 +44,39 @@ best <- function(state, outcome) {
 best("TX", "heart attack")
 
 
- 
 
+
+
+#Part 3  file: rankhospital.R
+
+rankhospital <- function(state, outcome, num = "best") {
+  
+  ## Read outcome data 
+    outcome1 <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
+    outcome1 <- outcome1[ , c(2,6,7,11, 17, 23)]
+    names(outcome1) <- c("Hospital", "City", "State", "heart failure", "heart attack", "pneumonia")
+
+  ## Check that state and outcome are valid
+    if(any(state == outcome1$State, na.rm = TRUE) == "FALSE") { stop("invalid state" )}
+    if(any(outcome == c("heart failure", "heart attack", "pneumonia"), na.rm = TRUE) == "FALSE") {
+    stop("invalid outcome") } 
+
+  ## Return hospital name in that state with the given rank
+    filter(outcome1, State == state) %>% select(c("Hospital", outcome)) %>% na.omit()-> outcome1
+    outcome1 %>% arrange(outcome1[ ,2], Hospital) -> outcome1
+    print(outcome1[1:5,])
+    
+    }
+  
+
+  
+  
+rankhospital("MD", "heart failure", 5)
+
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+  
 

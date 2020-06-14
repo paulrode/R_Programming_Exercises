d331f7e5ebde046ec1a9519ba62a7e2ea92f703c
paulrode <paulrode@outlook.com>
2020-06-14 09:48:48 -0400

    put in logic for last step

--- Assigement 3.R
+++ Assigement 3.R
@@ -95,8 +95,10 @@ rankall <- function(outcome, num = "best") {
   outcome1$State <- as.character(outcome1$State)
   outcome1$`heart attack` <- as.numeric(outcome1$`heart attack`)
   unique(outcome1$State) -> outcome2
-  outcome1 %>% group_by(State) %>% summarise( Answer = nth(`heart attack` , 1)) 
+  outcome1 %>% group_by(State) %>% summarise( Answer = nth(`heart attack` , num)) 
 
+  # trying apply
+  apply(rankhospital("MD", "heart failure", "worse"))
  
 
   

1f5b8d66a54c5dce5339140969ae06d04cd00b4a
paulrode <paulrode@outlook.com>
2020-06-14 21:22:39 -0400

    Final

--- rankhospital.R
+++ rankhospital.R
@@ -12,18 +12,19 @@ rankhospital <- function(state, outcome, num = "best") {
   outcome1 <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
   outcome1 <- outcome1[ , c(2,6,7,11, 17, 23)]
   names(outcome1) <- c("Hospital", "City", "State", "heart failure", "heart attack", "pneumonia")
-  
+
   ## Check that state and outcome are valid
-  if(any(state == outcome1$State, na.rm = TRUE) == "FALSE") { stop("invalid state" )}
-  if(any(outcome == c("heart failure", "heart attack", "pneumonia"), na.rm = TRUE) == "FALSE") {
+  if(any(state == outcome1$State, na.rm = FALSE) == "FALSE") { stop("invalid state" )}
+  if(any(outcome == c("heart failure", "heart attack", "pneumonia"), na.rm = FALSE) == "FALSE") {
     stop("invalid outcome") } 
   
   ## Return hospital name in that state with the given rank
-  filter(outcome1, State == state) %>% select(c("Hospital", outcome)) %>% na.omit()-> outcome1
+  filter(outcome1, State == state) %>% select(c("Hospital", outcome)) -> outcome1
   outcome1 %>% arrange(outcome1[ ,2], Hospital) -> outcome1
   if(num == "best") { num <- 1} 
   if(num == "worse") { num <- nrow(outcome1)}
   if(num > nrow(outcome1)) {print("NA")} else {print(outcome1[num, ])}
 }
+
 
 
